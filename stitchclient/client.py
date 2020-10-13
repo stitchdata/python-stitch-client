50e34a017727a22c552bd9d130f2629655d50a3b
John Miller <john.jj.miller@gmail.com>
2020-10-13 09:14:14 -0400

    Add region kwarg to Client, update README, and bump to 1.0.0 (#12)
    Co-authored-by: vagrant <vagrant@vagrant.vm>

--- stitchclient/client.py
+++ stitchclient/client.py
@@ -6,13 +6,17 @@ import time
 import requests
 from transit.writer import Writer
 
+
 DEFAULT_MAX_BATCH_SIZE_BYTES = 4194304
 DEFAULT_BATCH_DELAY_SECONDS = 60.0
 MAX_MESSAGES_PER_BATCH = 20000
 DEFAULT_STITCH_URL = 'https://api.stitchdata.com/v2/import/push'
+EU_STITCH_URL = 'https://api.eu-central-1.stitchdata.com/v2/import/push'
+
 
 class MessageTooLargeException(Exception):
     pass
+
 
 def encode_transit(records):
     '''Returns the records serialized as Transit/json in utf8'''
@@ -23,12 +27,10 @@ def encode_transit(records):
 
 
 def partition_batch(entries, max_batch_size_bytes):
-
     start = 0
     end = len(entries)
     result = []
     while start < end:
-
         partitioned_entries = entries[start : end]
         records = [e.value for e in partitioned_entries]
         encoded = encode_transit(records)
@@ -55,39 +57,50 @@ def partition_batch(entries, max_batch_size_bytes):
 
         else:
             raise MessageTooLargeException(
-                ('A single message is larger then the maximum batch size. ' +
+                ('A single message is larger then the maximum batch size. '
                  'Message size: {}. Max batch size: {}')
                 .format(len(encoded), max_batch_size_bytes))
 
     return result
 
+
 BufferEntry = collections.namedtuple(
     'BufferEntry',
-    ['value', 'callback_arg'])
+    ['value', 'callback_arg'],
+)
+
 
 BatchStatsEntry = collections.namedtuple(
-    'BatchStatsEntry', ['num_records', 'num_bytes'])
+    'BatchStatsEntry',
+    ['num_records', 'num_bytes'],
+)
+
 
 class Client(object):
+    def __init__(
+            self,
+            client_id,
+            token,
+            region="us",
+            table_name=None,
+            key_names=None,
+            callback_function=None,
+            override_stitch_url=None,
+            max_batch_size_bytes=DEFAULT_MAX_BATCH_SIZE_BYTES,
+            batch_delay_seconds=DEFAULT_BATCH_DELAY_SECONDS):
 
-    def __init__(self,
-                 client_id,
-                 token,
-                 table_name=None,
-                 key_names=None,
-                 callback_function=None,
-                 stitch_url=DEFAULT_STITCH_URL,
-                 max_batch_size_bytes=DEFAULT_MAX_BATCH_SIZE_BYTES,
-                 batch_delay_seconds=DEFAULT_BATCH_DELAY_SECONDS):
+        try:
+            self.client_id = int(client_id)
+        except:
+            raise ValueError("client_id must be an integer: %s.".format(client_id))
 
-        assert isinstance(client_id, int), 'client_id is not an integer: {}'.format(client_id)  # nopep8
+        if not token:
+            raise ValueError("token is null")
 
-        self.max_messages_per_batch = MAX_MESSAGES_PER_BATCH
-        self.client_id = client_id
         self.token = token
+        self.max_messages_per_batch = MAX_MESSAGES_PER_BATCH
         self.table_name = table_name
         self.key_names = key_names
-        self.stitch_url = stitch_url
         self.max_batch_size_bytes = max_batch_size_bytes
         self.batch_delay_seconds = batch_delay_seconds
         self.callback_function = callback_function
@@ -101,9 +114,21 @@ class Client(object):
         # We'll try using a big batch size to start out
         self.target_messages_per_batch = self.max_messages_per_batch
 
+        if override_stitch_url:
+            self.stitch_url = override_stitch_url
+        elif region.lower() == "us":
+            self.stitch_url = DEFAULT_STITCH_URL
+        elif region.lower() == "eu":
+            self.stitch_url = EU_STITCH_URL
+        else:
+            raise ValueError(
+                "Invalid region selected: %s. "
+                "Options are 'us' and 'eu' or set the override_stitch_url."
+                .format(region)
+            )
+
     def _add_message(self, message, callback_arg):
-        self._buffer.append(BufferEntry(value=message,
-                                        callback_arg=callback_arg))
+        self._buffer.append(BufferEntry(value=message, callback_arg=callback_arg))
 
     def moving_average_bytes_per_record(self):
         num_records = 0
@@ -132,7 +157,6 @@ class Client(object):
         if batch:
             self._send_batch(batch)
 
-
     def _take_batch(self, min_records):
         '''If we have enough data to build a batch, returns all the data in the
         buffer and then clears the buffer.'''
@@ -151,7 +175,6 @@ class Client(object):
         self._buffer.clear()
         return result
 
-
     def _send_batch(self, batch):
         for body, callback_args in partition_batch(batch, self.max_batch_size_bytes):
             self._send(body, callback_args)
@@ -165,12 +188,12 @@ class Client(object):
             # Handle the case where there are no records
             pass
 
-
     def _stitch_request(self, body):
-        headers = {'Authorization': 'Bearer {}'.format(self.token),
-                   'Content-Type': 'application/transit+json'}
+        headers = {
+            'Authorization': 'Bearer {}'.format(self.token),
+            'Content-Type': 'application/transit+json',
+        }
         return requests.post(self.stitch_url, headers=headers, data=body)
-
 
     def _send(self, body, callback_args):
         response = self._stitch_request(body)
@@ -179,8 +202,9 @@ class Client(object):
             if self.callback_function is not None:
                 self.callback_function(callback_args)
         else:
-            raise RuntimeError("Error sending data to the Stitch API. {0.status_code} - {0.content}"  # nopep8
-                               .format(response))
+            raise RuntimeError(
+                "Error sending data to the Stitch API. {0.status_code} - {0.content}"
+                .format(response))
         self.time_last_batch_sent = time.time()
         self.batch_stats.append(BatchStatsEntry(len(callback_args), len(body)))
 
@@ -196,13 +220,20 @@ class Client(object):
 
 
 if __name__ == "__main__":
-
-    with Client(int(os.environ['STITCH_CLIENT_ID']),
-                os.environ['STITCH_TOKEN'],
-                callback_function=print) as c:
+    with Client(
+            os.environ.get('STITCH_CLIENT_ID'),
+            os.environ.get('STITCH_TOKEN'),
+            os.environ.get('STITCH_REGION'),
+            callback_function=print,
+    ) as client:
         for i in range(1, 10):
-            c.push({'action': 'upsert',
-                    'table_name': 'test_table',
-                    'key_names': ['id'],
-                    'sequence': i,
-                    'data': {'id': i, 'value': 'abc'}}, i)
+            client.push({
+                'action': 'upsert',
+                'table_name': 'test_table',
+                'key_names': ['id'],
+                'sequence': i,
+                'data': {
+                    'id': i,
+                    'value': 'abc',
+                },
+            }, i)
